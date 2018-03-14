f991e8fea3d77cf0b871847f1eed4c1e8bf668b9
tanglingpig <cfabian150@gmail.com>
2018-03-14 15:20:35 +0000

    file name is now an argument of main

--- main.java
+++ main.java
@@ -1,10 +1,23 @@
 import markovModel.*;
+import java.io.File;
 class main
 {
     public static void main(String[] args)
     {
-        MarkovModel a = new MarkovModel<>("test.txt");
-        String text = a.generateText(20);
-        System.out.printf("%s\n", text);
+        if(args.length > 0) {
+            File f = new File(args[0]);
+            if(f.exists() && !f.isDirectory()) {
+                MarkovModel a = new MarkovModel<>(args[0]);
+                String text = a.generateText(20);
+                System.out.printf("%s\n", text);
+            } else {
+                System.out.printf("%s is not a valid file.\n", args[0]);
+                return;
+            }
+        } else {
+            MarkovModel a = new MarkovModel<>("test.txt");
+            String text = a.generateText(20);
+            System.out.printf("%s\n", text);
+        }
     }
 }
